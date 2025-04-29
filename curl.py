ed6c2aa7189e04e28f44e63cb4389781bc63191f
Ivan Fedotikov <idfedotikov@gmail.com>
2025-04-29 18:20:58 +0300

    version 0.3

--- curl.py
+++ curl.py
@@ -4,7 +4,7 @@ service_url = 'https://stellarburgers.nomoreparties.site/'
 
 
 login_url = f'{service_url}login'
-register_url = f'//{service_url}register'
+register_url = f'{service_url}register'
 personal_account_url = f'{service_url}account/profile'
 
 forgot_password_url = f'{service_url}forgot-password'
--- test/test_registration.py
+++ test/test_registration.py
@@ -7,8 +7,6 @@ from helper import generate_registration_data
 from curl import *
 
 
-
-
 class TestRegistration:
     def test_success_registration(self, driver):
 
@@ -32,19 +30,12 @@ class TestErrorRegistration:
     def test_success_registration(self, driver):
 
         name, email, password = generate_registration_data(pass_error=True)
-
-
         driver.find_element(*Locators.LOGIN_MAIN_BUTTON).click()
         driver.find_element(*Locators.REG_BUTTON).click()
         driver.find_element(*Locators.NAME_FIELD).send_keys(name)
         driver.find_element(*Locators.EMAIL_FIELD).send_keys(email)
         driver.find_element(*Locators.PASSWORD_FIELD).send_keys(password)
         driver.find_element(*Locators.CONTINUE_REG_BUTTON).click()
-
-        WebDriverWait(driver, 15).until(
-            EC.element_to_be_clickable(Locators.LOGIN)
-        )
-
 
         assert WebDriverWait(driver, 1).until(EC.visibility_of_element_located(Locators.INVALID_PASSWORD_MESSAGE)).text == 'Некорректный пароль'
         assert driver.current_url == register_url
